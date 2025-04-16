435c89e26f2272bdcf3d1a6b2358d182b5b780ac
nniklas <46904141+nniklas@users.noreply.github.com>
2025-04-16 20:05:24 +0200

    create subtract function

--- java.js
+++ java.js
@@ -3,3 +3,11 @@ function add(a,b){
     // make sure to create numbers from strings
     return +a + +b;
 };
+
+
+// subtract function
+// add function
+function subtract(a,b){
+    // make sure to create numbers from strings
+    return +a - +b;
+};
